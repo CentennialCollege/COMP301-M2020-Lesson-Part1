840289e31ec381498721d4fbaf855e2f79381cd9
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-07-06 18:10:13 -0400

    demo'd functions and arrays

--- first.py
+++ first.py
@@ -1,41 +1,28 @@
 
-#this is a comment!
-a, b, c = 20, 10, None # two variables
+weekDays = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-A, B, C = 20, 30, 40
+for day in weekDays:
+    print(day)
 
-
-print("Hello, World!")
-
-print(f"hello a: {a} b: {b}")
-
-if (a < b):
-    print("a is less than b")
-elif(a > b):
-    print("a is greater than b")
-else:
-    print("a is equal to b")
-
-if(a == A):
-    print("a is equal to A")
-else:
-    print("a is not equal to A")
+print()
+print(weekDays[0])
 
 
-for i in range(0, 6):
-    print(i)
+# functions need to be defined before we use them
+def sayHello():
+    print()
+    print("Hello, World!")
 
 
-count = 0 # sentinel variable
+sayHello()
 
-while count < 10:
-    print(count)
-    count+=1
 
-myArray = [1, 3, 5, 7, 9]
 
-for element in myArray:
-    print(element)
+
+
+
+
+
 
 
 
