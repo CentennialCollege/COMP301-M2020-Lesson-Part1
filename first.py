3f80b02305a1d55e262f586a46f2a852d0f7afa4
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-07-06 23:46:32 -0400

    added months variable

--- first.py
+++ first.py
@@ -7,6 +7,8 @@
 
 
 weekDays = ["Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
 
 for day in weekDays:
     print(day)
