6dd143c94baa43fdcdaf53627ca39edbabb219a6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-07-09 14:57:44 -0400

    added second.py

---
+++ second.py
@@ -0,0 +1,30 @@
+a = 10
+b = 20
+c = a + b
+
+e = a ** 0.5
+
+print(f"{a} + {b} = {c}", end="\n")
+print(" is a great result!")
+
+print(e)
+
+floor = 5 // 2
+print(floor)
+
+remainder = 5 % 2
+print(remainder)
+
+print("a is equal to: " + str(a))
+
+#print("Enter your name: ", end="")
+name = input("Enter your name: ")
+print("You typed: " + name)
+
+xString = input("Enter the x value: ")
+x = float(xString)
+yString = input("Enter the y value: ")
+y = float(yString)
+
+print(f"({x}, {y})")
+
